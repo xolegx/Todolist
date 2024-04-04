92b04ec9515d2252465ee02d0beede2454c3351a
Хизриев Олег В <ohizriev@grc.ru>
2024-04-04 14:06:57 +0500

    'create_user_serial'

--- first/serializers.py
+++ first/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from first.models import User
 
 class PasswordField(serializers.CharField):
     def __init__(self, **kwargs):
@@ -7,3 +8,9 @@ class PasswordField(serializers.CharField):
         kwargs.setdefault('write_only', True)
         super().__init__(**kwargs)
         self.validators.append(validate_password)
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = PasswordField(required=True)
+    password_repeate = PasswordField(required=True)
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'password_repeate')
