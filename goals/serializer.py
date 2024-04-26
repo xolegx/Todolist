1c2124663f3cd5502b03c070268d932bbb53089f
Хизриев Олег В <ohizriev@grc.ru>
2024-04-26 16:11:20 +0500

    'goal_ser

--- goals/serializer.py
+++ goals/serializer.py
@@ -1,6 +1,13 @@
+from typing import Type
+
+from django.db import transaction
 from rest_framework import serializers
-from goals.models import GoalCategory
+from rest_framework.exceptions import PermissionDenied
+
+from first.models import User
 from first.serializers import ProfileSerializer
+from goals.models import GoalCategory, Goal
+
 
 class GoalCategoryCreateSerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
@@ -17,3 +24,22 @@ class GoalCategorySerializer(serializers.ModelSerializer):
         model = GoalCategory
         fields = "__all__"
         read_only_fields = ("id", "created", "updated", "user", "board")
+
+class GoalCreateSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=GoalCategory.objects.filter(is_deleted=False)
+    )
+
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Goal
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
+
+        def validate_category(self, value: GoalCategory):
+            if self.context['request'].user != value.user:
+                raise PermissionDenied
+
+            return value
+
